c77a120a7b3f68596f5a4245bea29c903fb52cb9
Shalk000 <Shalk000@209-11>
2024-10-04 14:38:00 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+try
+{
+    Console.WriteLine("Введите число n>999");
+    int n = int.Parse(Console.ReadLine());
+    double a = n / 100;
+    Console.WriteLine($"вот столько сотен в n{a}");
+    double b = n / 1000;
+    Console.WriteLine($"вот столько тысяч в n {b}");
+}
+catch
+{
+    Console.WriteLine("Error");
+}
